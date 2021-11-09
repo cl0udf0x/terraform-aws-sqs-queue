bf15ca30c532e4497da05b209180610b9bdb0264
nitrocode <nitrocode@users.noreply.github.com>
2021-11-09 17:39:34 -0600

    Additional outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,16 @@
-output "id" {
+output "url" {
   description = "The URL for the created Amazon SQS queue."
+  value       = local.enabled ? aws_sqs_queue.default[0].url : null
+}
+
+output "id" {
+  description = "The ID for the created Amazon SQS queue. Same as the URL."
   value       = local.enabled ? aws_sqs_queue.default[0].id : null
+}
+
+output "name" {
+  description = "The name for the created Amazon SQS queue."
+  value       = local.enabled ? module.this.id : null
 }
 
 output "arn" {
